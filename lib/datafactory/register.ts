b6927bc6c32cbaf7872a0d6fbbe4b92d66250219
marcosdaniel <marcosdaniel@outlook.com>
2025-12-01 17:21:58 -0300

    first commit

---
+++ lib/datafactory/register.ts
@@ -0,0 +1,26 @@
+import { request, expect } from '@playwright/test';
+
+export async function registerUser(email: string, password: string) {
+  const apiURL = process.env.API_URL;
+  console.log('Api Url: ', apiURL);
+  const userCreations = await request.newContext();
+  const response = await userCreations.post(apiURL + '/users/register', {
+    data: {
+      first_name: 'Test',
+      last_name: 'User',
+      dob: '1987-01-01',
+      phone: '5555555555',
+      email: email,
+      password: password,
+      address: {
+        street: '101 Testing Way',
+        city: 'New York',
+        state: 'New York',
+        country: 'US',
+        postal_code: '55555',
+      },
+    },
+  });
+  expect(response.status()).toBe(201);
+  return response.status();
+}
---
+++ tests/messages.spec.ts
@@ -0,0 +1,49 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '@pages/login/loginpage';
+import { registerUser } from '@datafactory/register';
+import { createMessage } from '@datafactory/createMessage';
+import { MessagesPage } from '@pages/account/messages.page';
+
+test('Should create a new user and send a message to support and conffirms that message is displayed correctly', async ({
+  page,
+  context,
+}) => {
+  const timestamp = Date.now();
+  const email = `newuser${timestamp}@test.com`;
+  const password = '123@Da12551';
+  const dropdownOptions = 'payments';
+  const message =
+    'This is a really long message that goes on an on for at least 50 characters';
+  const messageUserAuthFile = '.auth/messageUser.json';
+  await test.step('create new user', async () => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await registerUser(email, password);
+    await loginPage.login(email, password);
+    await expect(page.locator('[data-test="nav-menu"]')).toContainText(
+      'Test User'
+    );
+    await expect(
+      page.evaluate(() => localStorage.getItem('auth-token'))
+    ).not.toBeNull();
+    await context.storageState({ path: messageUserAuthFile });
+  });
+
+  await test.step('Creating a message', async () => {
+    await createMessage(
+      'Testy Mctesterface',
+      message,
+      dropdownOptions,
+      messageUserAuthFile
+    );
+  });
+  await test.step('Verify the existency of the message', async () => {
+    const messagesPage = new MessagesPage(page);
+    await messagesPage.goto();
+    await expect(messagesPage.table).toContainText(message.substring(0, 25));
+    await expect(messagesPage.table).toContainText(dropdownOptions);
+
+    await messagesPage.firstDetailLink.click();
+    await expect(messagesPage.messagesList).toContainText(message);
+  });
+});
